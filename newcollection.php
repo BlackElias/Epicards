aa2c6db05ebc445c3185892df471979a1323ddeb
BlackElias <valienne.elias@outlook.com>
2022-05-10 22:41:34 +0200

    added flox pages

---
+++ newcollection.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>new collection</title>
+</head>
+<body>
+    
+<form action="">
+<h2>Private collection</h2>
+
+  <input type="checkbox">
+  <span class="slider"></span>
+
+<label for="">Collection Name</label>
+<input type="text">
+
+
+<h3>type collection</h3>
+<input type="submit"><img src="" alt=""></img></input>
+<input type="submit"><img src="" alt=""></img></input>
+<input type="submit"><img src="" alt=""></img></input>
+</form>
+</body>
+</html>
