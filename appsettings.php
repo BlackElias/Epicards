1e910554a19bab7b58d7de872f7371544a8d6f19
kevin <kevin.vanbockryck@hotmail.com>
2022-05-14 00:52:55 +0200

    settings + logout

---
+++ appsettings.php
@@ -0,0 +1,26 @@
+<?php
+include_once("bootstrap.php");
+include_once("header.inc.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/appsettings.css">
+    <title>Settings</title>
+</head>
+
+<body>
+    <div class="containter_appsettings"> 
+        <a href="index.php"><img src="assets/back_arrow.svg" alt="back arrow" class="back_arrow"></a>
+        <h1>Settings</h1>
+        <button class="btn logout_btn"><a href="logout.php">logout</a></button>
+    </div>
+</body>
+
+</html>
