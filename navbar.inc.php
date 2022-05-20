fc6fbf93aa4cd69016cb469a3354a7484b488b9d
kevin <kevin.vanbockryck@hotmail.com>
2022-05-20 13:02:39 +0200

    css

---
+++ navbar.inc.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="navbar_container">
+        <div class="nav_icon collection_icon"></div>
+        <div class="nav_icon trade_sell_icon"></div>
+        <div class="nav_icon chat_icon"></div>
+        <div class="nav_icon profile_icon"></div>
+    </div>
+
+</body>
+
+</html>
