5acdaec5e74f47bbe8f146e85328fa271818f9d4
kevin <kevin.vanbockryck@hotmail.com>
2022-05-20 14:39:57 +0200

    collection cards styling

--- collection.inc.php
+++ collection.inc.php
@@ -3,13 +3,19 @@ include_once("bootstrap.php");
 
 ?>
 
-<a class="collection-text" style="text-decoration: none; color:black" href="collection.php?title=<?php echo $collection["collection_name"] ?>&id=<?php echo $collection["collection_id"] ?>">
-<div class="container">
-    <div class="collection-pokemon">
-        <img src="" alt=""></img>
-        <p>cards</p>
-        <p><?php echo htmlspecialchars($collection['collection_name']) ?></p>
-    </div>
-    </a>
+<a class="collection-text" href="collection.php?title=<?php echo $collection["collection_name"] ?>&id=<?php echo $collection["collection_id"] ?>">
+    <div class="container">
+        <div class="collection-pokemon">
+            <div class="collection_card_text">
+                <div class="cards_amount">
+                    <p>0</p>
+                    <p class="">cards</p>
+                </div>
+               
+                <p class="collection_title"> <img src="assets/card_icon.svg" class="card_icon" alt=""><?php echo htmlspecialchars($collection['collection_name']) ?></p>
+            </div>
 
-    </div>
+        </div>
+</a>
+
+</div>
