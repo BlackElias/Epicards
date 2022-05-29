9934cca3db77e876253451d11a5be454a5d88c94
kevin <kevin.vanbockryck@hotmail.com>
2022-05-29 18:28:21 +0200

    restricted resolution overlay

--- navbar.inc.php
+++ navbar.inc.php
@@ -11,25 +11,33 @@
 </head>
 
 <body>
-    <div class="bar_position">
-        <div id="myDiv">
-            <div class="bttm_navbar ">
-                <div class="nav_item active">
-                    <a href="index.php"> <img src="assets/archive_icon.svg" alt="archive icon" class="nav_icons archive_icon"></a><span class="nav_text collection_text">Collection</span>
-                </div>
-                <div class="nav_item">
-                    <a href="trade_sell.php"><img src="assets/trade_sell_icon.svg" alt="archive icon" class="nav_icons trade_sell_icon"></a><span class="nav_text trade_sell_text">Trade & Sell</span>
-                </div>
-                <div class="nav_item">
-                    <a href="chat.php"> <img src="assets/chat_icon.svg" alt="archive icon" class="nav_icons chat_icon"></a><span class="nav_text chat_text">Chat</span>
-                </div>
-                <div class="nav_item">
-                    <a href="profile.php"> <img src="assets/profile_icon.svg" alt="archive icon" class="nav_icons profile_icon"></a><span class="nav_text profile_text">Profile</span>
-                </div>
-
+    <div id="overlay">
+        <div class="overlay_contents">
+            <img src="assets/logo.png" alt="logo" class="overlay_logo">
+            <p class="overlay_text">Unfortunately, Epicards is not yet fully optimised for this device. <br><br> Please use your smartphone instead.</p>
+            <div class="overlay_icon">
+                <img src="assets/monitor.svg" alt="monitor icon" class="monitor_icon">
+                <img src="assets/smartphone.svg" alt="monitor icon" class="mobile_icon">
             </div>
         </div>
     </div>
+    <div class="bar_position">
+        <div class="bttm_navbar ">
+            <div class="nav_item active">
+                <a href="index.php"> <img src="assets/archive_icon.svg" alt="archive icon" class="nav_icons archive_icon"></a><span class="nav_text collection_text">Collection</span>
+            </div>
+            <div class="nav_item">
+                <a href="trade_sell.php"><img src="assets/trade_sell_icon.svg" alt="archive icon" class="nav_icons trade_sell_icon"></a><span class="nav_text trade_sell_text">Trade & Sell</span>
+            </div>
+            <div class="nav_item">
+                <a href="chat.php"> <img src="assets/chat_icon.svg" alt="archive icon" class="nav_icons chat_icon"></a><span class="nav_text chat_text">Chat</span>
+            </div>
+            <div class="nav_item">
+                <a href="profile.php"> <img src="assets/profile_icon.svg" alt="archive icon" class="nav_icons profile_icon"></a><span class="nav_text profile_text">Profile</span>
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
