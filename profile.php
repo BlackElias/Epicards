a6b26b069fc20ee0233ed1a291bc41e2ca5176df
kevin <kevin.vanbockryck@hotmail.com>
2022-06-07 04:50:23 +0200

    trade_sell, profile styling + fixes

--- profile.php
+++ profile.php
@@ -13,11 +13,10 @@ try {
     $currentUserId = $_SESSION["userId"];
     $currentUser = $user->getUserInfo($currentUserId);
 
-  
 
-        $currentUser = $user->getUserInfo($currentUserId); //---Updated User Fetch---
-    }
- catch (\Throwable $th) {
+
+    $currentUser = $user->getUserInfo($currentUserId); //---Updated User Fetch---
+} catch (\Throwable $th) {
     $error = $th->getMessage();
 }
 
@@ -86,6 +85,69 @@ try {
     foreach ($feed as $post) :  ?>
         <?php include("post.inc.php") ?>
     <?php endforeach; ?>
+        <div class="profile-box-info">
+            <?php if (!empty($currentUser["picture"])) : ?>
+                <img class="profile-picture-big" src="<?php echo $currentUser["picture"] ?>" alt="profile picture">
+            <?php endif; ?>
+            <div class="profile-box-names">
+                <h1><?php echo htmlspecialchars($currentUser["username"])?></h1>
+            </div>
+        </div>
+</body>
+
+
+<main>
+    <div class="box-container">
+        <div class="profile-box">
+
+        </div>
+
+    </div>
+    </div>
+    </div>
+    <div class="profile-stats-container">
+        <div class="box-container-medium ">
+            <div class="profile-stats-box">
+
+
+            </div>
+        </div>
+
+
+</main>
+<?php
+
+
+
+
+$i = 0;
+foreach ($feed as $collection) : if ($i == 20) {
+
+        break;
+    } ?>
+
+    <a class="collection-text" href="collection.php?title=<?php echo $collection["collection_name"] ?>&id=<?php echo $collection["collection_id"] ?>&type=<?php echo $collection["collection_type"] ?>">
+        <div class="container">
+            <div class="collection-pokemon">
+                <div class="collection_card_text">
+                    <div class="cards_amount">
+                        <p><?php
+
+                            echo Collection::countCards($collection["collection_id"]);
+                            ?></p>
+                        <p class="">cards</p>
+                    </div>
+
+                    <p class="collection_title"> <img src="assets/card_icon.svg" class="card_icon" alt=""><span class="coll_title-text"><?php echo htmlspecialchars($collection['collection_name']) ?></span></p>
+                </div>
+            </div>
+        </div>
+    </a>
+<?php $i++;
+endforeach;  ?>
+
+
+
 </body>
 
 </html>
--- trade_sell.php
+++ trade_sell.php
@@ -3,27 +3,206 @@
 include_once("bootstrap.php");
 include_once("header.inc.php");
 include_once("navbar.inc.php");
+try {
+  $user = new User();
+  $currentUserId = $_SESSION["userId"];
+  $currentUser = $user->getUserInfo($currentUserId);
+} catch (\Throwable $th) {
+  $error = $th->getMessage();
+}
+
+if (!empty($_POST["cardName"])) {
+
+  try {
+    $card = new Cards();
+    $card->setCollectionId($_POST['id']);
+
+    $card->setCard_name($_POST["cardName"]);
+    $card->setCard_price($_POST["cardPrice"]);
+    $card->setCard_image($_POST["cardImage"]);
+
+
+
+    $card->saveCards();
+  } catch (\Throwable $th) {
+    $error = $th->getMessage();
+  }
+}
+
+
+
+if (!empty($_GET['query'])) {
+  try {
+    $user = new User;
+
+    $searchresult = $_GET['query'];
+
+
+    $users = $user->searchusers($searchresult);
+  } catch (\Throwable $th) {
+    $error = $th->getMessage();
+  }
+}
+if (!empty($_GET['shops'])) {
+  try {
+    $user = new User;
+
+    $searchresult = $_GET['shops'];
+
+
+    $users = $user->searchShop($searchresult);
+  } catch (\Throwable $th) {
+    $error = $th->getMessage();
+  }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/trade_sell.css">
-    <link rel="stylesheet" href="css/bottom-navbar/trade_sell_bar.css">
-    <title>Trade and sell</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/trade_sell.css">
+  <link rel="stylesheet" href="css/bottom-navbar/trade_sell_bar.css">
 </head>
 
 <body>
-    <div class="top">
-        <button onclick="history.go(-1);"><img src="assets/back_arrow.svg" alt="back arrow" class="back_arrow"></button>
-        <h1>Trade and sell</h1>
-    </div>
+  <div class="top">
+    <button onclick="history.go(-1);"><img src="assets/back_arrow.svg" alt="back arrow" class="back_arrow"></button>
+    <h1>Trade and sell</h1>
+  </div>
 </body>
+<div class="input-field col s12">
+  <div class="top-flex">
+    <div class="search">
+      <?php if (isset($_GET["cards"])) {
+        echo   '<input type="text" id="name-input" placeholder="Search for cards" name="query" name="current-search" class="form_input card_input">';
+        echo   '<button id="search-button" class="search_btn"><img src="assets/search_icon.svg" alt="search button" class="search_btn"></button>';
+      } else if (isset($_GET["shops"])) {
+        echo  '<form action="" method="get">';
+        echo  '<input type="text" id="name-input" placeholder="Search for shops" name="shops" name="current-search" class="form_input card_input">';
+        echo  '<button id="search-button" class="search_btn"><img src="assets/search_icon.svg" alt="search button" class="search_btn"></button></form>';
+      } else {
+        echo  '<form action="" method="get">';
+        echo  '<input type="text" id="name-input" placeholder="Search for other users" name="query" name="current-search" class="form_input card_input">';
+        echo  '<button id="search-button" class="search_btn"><img src="assets/search_icon.svg" alt="search button" class="search_btn"></button></form>';
+      } ?>
+      <div class="search_category">
+        <button><a href="trade_sell.php">people</a></button>
+        <span class="vl_line"></span>
+        <button><a href="trade_sell.php?shops=">shops</a></button>
+        <span class="vl_line"></span>
+        <button class=""><a href="trade_sell.php?cards=">cards</a></button>
+      </div>
+    </div>
+  </div>
+
+  <select class="browser-default" id="generation-search" style="display:none ;">
+    <option value="">Choose your option</option>
+    <option value="generation/1">1</option>
+    <option value="generation/2">2</option>
+    <option value="generation/3">3</option>
+    <option value="generation/4">4</option>
+    <option value="generation/5">5</option>
+    <option value="generation/6">6</option>
+    <option value="generation/7">7</option>
+  </select>
+
+
+  <select class="browser-default" id="type-search" style="display:none ;">
+    <option value="">Choose your option</option>
+    <option value="Grass">Grass</option>
+    <option value="Fire">Fire</option>
+    <option value="Water">Water</option>
+    <option value="Lightning">Lightning</option>
+    <option value="Psychic">Psychic</option>
+    <option value="Fighting">Fighting</option>
+    <option value="Darkness">Darkness</option>
+    <option value="Metal">Metal</option>
+    <option value="Dragon">Dragon</option>
+    <option value="Fairy">Fairy</option>
+    <option value="Colorless">Colorless</option>
+  </select>
+
+
+</div>
+</ul>
+</div>
+
+<div class="card_scroll">
+  <div>
+    <div class="col s12 m8 l9">
+      <!-- Teal page content  -->
+      <div class="row responsive-img" id="pokeResults">
+
+      </div>
+    </div>
+  </div>
+  <?php if (isset($users)) {
+    foreach ($users as $searchresult) :  ?>
+
+      <a class="search_text" href="people.php?id=<?php echo $searchresult['id']; ?>&username=<?php echo $searchresult['username']; ?>">
+        <div class="searchresult"><?php echo $searchresult['username']; ?> </div>
+      </a>
+
+  <?php endforeach;
+  } ?>
+  <!-- The Modal -->
+  <div id="myModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+      <span onclick="document.getElementById('myModal').style.display='none'" class="close">&times; close</span>
+      <h4 id="modal-card-name"></h4>
+      <p id="modal-card-type"></p>
+      <table class="responsive-table highlight">
+        <p id="test"></p>
+
+        <tbody>
+          <tr>
+            <td id="priceH">Holofoil Market Price</td>
+            <td id="modal-HoloFoil-price"></td>
+          </tr>
+          <tr>
+            <td id="priceR">Reverse Holofoil Market Price</td>
+            <td id="modal-reverseHolofoil-price"></td>
+
+          </tr>
+          <tr>
+            <td>Normal Market Price</td>
+            <td id="modal-normal-price"></td>
+
+          </tr>
+        </tbody>
+      </table>
+      <img class="responsive-img" src="" alt="" id="modal-card-image">
+      <form action="" method="">
+
+        <input id="addCard-data" type="hidden" value="" name="cardName"></input>
+        <input id="addCard-price" type="hidden" value="" name="cardPrice"></input>
+        <input id="addCard-image" type="hidden" value="" name="cardImage"></input>
+        <link rel="stylesheet" href="css/bottom-navbar/trade_sell_bar.css">
+      </form><input type="hidden" value="" name="id"></input>
+
+      <button class="btn"><a id="buyCard" href="">buy</a> </button>
+
+
+    </div>
+  </div>
+</div>
+<!-- The Modal -->
+<?php if (isset($_GET["cards"])) {
+  echo '<script src="src/js/search.js"></script>';
+} ?>
+
+>>>>>>> Stashed changes
 </body>
 
 </html>
