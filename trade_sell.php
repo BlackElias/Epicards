85044ee580f10d08ef94e4b4caca4e933a46ed30
Cocytos97 <driesjacobs007@gmail.com>
2022-06-12 16:49:37 +0200

    Post update

--- trade_sell.php
+++ trade_sell.php
@@ -1,8 +1,12 @@
 <?php
+use src\php\classes\Trade\Trade;
+use src\php\classes\Cards\Cards;
+
 include_once("bootstrap.php");
+
+
 include_once("header.inc.php");
 include_once("navbar.inc.php");
-
 ?>
 <!DOCTYPE html>
 
@@ -17,6 +21,7 @@ include_once("navbar.inc.php");
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   <link rel="stylesheet" href="css/trade_sell.css">
+  <link rel="stylesheet" href="css/index.css">
   <link rel="stylesheet" href="css/styles.css">
   <link rel="stylesheet" href="css/bottom-navbar/trade_sell_bar.css">
   <link rel="icon" type="image/x-icon" href="/upload/favicon.png">
@@ -37,11 +42,53 @@ include_once("navbar.inc.php");
       <span class="vl_line"></span>
       <a href="trade_sell-shops.php" class="shops_link">Shops</a>
     </div>
-    <div class="empty_state">
-      <img src="assets/emptystate_no-friends.svg" alt="empty state illustration" class="empty_state_img">
-      <p class="empty_state_text">You don't have friends added yet. <br> Search for friends in the search bar above!</p>
-    </div> <button class="btn-collection button_sec"><a href="newpost.php"><img src="assets/plus_icon.svg" alt="plus icon" class="icon_plus">new post</a></button>
 
+    <?php
+    $trade = Trade::getTradeByUserId($_SESSION["userId"]);
+            if (sizeof($trade) <= "0") {
+
+                echo '  
+        <div class="empty_state">
+            <img src="assets/empty_state_img.svg" alt="empty state illustration" class="empty_state_img"> 
+            <p class="empty_state_text">There are no cards in this collection</p>
+        </div>
+        ';
+            } ?>
+      
+      <div class="collections_scroll">
+      <?php
+
+         
+         //var_dump($currentUserId);
+
+         $i = 0;
+         foreach ($trade as $post) : if ($i == 20) {
+
+               break;
+            } ?>
+
+            <a class="post-text" href="trade.php?id=<?php echo $post["id"] ?>">
+               <div class="container">
+                  <div class="collection-pokemon">
+                     <div class="collection_card_text">
+                        <div class="cards_amount">
+                           <p><?php
+
+                              echo Trade::count($trade["id"]);
+                              ?></p>
+                           <p class="">cards</p>
+                        </div>
+
+                        <p class="collection_title"> <img src="assets/card_icon.svg" class="card_icon" alt=""><span class="coll_title-text"><?php echo htmlspecialchars($post['name']) ?></span></p>
+                     </div>
+                  </div>
+               </div>
+            </a>
+         <?php $i++;
+         
+         endforeach;  ?>
+         <div class="hidden_block"></div>
+      </div>
   </div>
 
 </body>
