1c2d13fdec803fba0d4f652f8d5bac58ac4a139b
kevin <kevin.vanbockryck@hotmail.com>
2022-06-15 14:49:48 +0200

    final stylings

--- appsettings.php
+++ appsettings.php
@@ -24,26 +24,54 @@ include_once("navbar.inc.php");
             <button onclick="history.go(-1);"><img src="assets/back_arrow.svg" alt="back arrow" class="back_arrow"></button>
             <h1>Settings</h1>
         </div>
-        <div class="settings_btn">
-            <div class="one_btn">
-                <img src="assets/notification.svg" alt="">
-                <a href="#">Notifications</a>
+        <div class="settings_scroll">
+            <div class="settings_btn">
+                <div class="one_btn">
+                    <img src="assets/notification.svg" alt="">
+                    <a href="#">Notifications</a>
+                </div>
+                <div class="one_btn">
+                    <img src="assets/globe.svg" alt="">
+                    <a href="#">Language and region</a>
+                </div>
+                <div class="one_btn">
+                    <img src="assets/dollar-sign.svg" alt="">
+                    <a href="#">Currency</a>
+                </div>
+                <div class="one_btn">
+                    <img src="assets/credit-card.svg" alt="">
+                    <a href="#">Payments</a>
+                </div>
+
+                <div class="flex_delete_btn">
+                    <a href="#popup1" class="delete_btn">
+                        <div class="one_btn">
+                            <img src="assets/info (1).svg" alt="">
+                            <span>Contact</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div id="popup1" class="overlay">
+                    <div class="popup">
+                        <a class="close" href="#" class="button_sec">&times;</a>
+                        <h2>Send us an email!</h2>
+                        <p>Do you have any questions or feedback? <br> Or are you interested in working together? <br> Please don't hesitate and send us an email!</p>
+                        <div class="content">
+                            <a href="mailto:epicards@eliasv.be" class="button_sec">Contact us</a>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
-            <div class="one_btn">
-                <img src="assets/globe.svg" alt="">
-                <a href="#">Language and region</a>
+
+            <div class="container">
+                <a href="logout.php" class="btn" style="text-align:center; width: 90px;">logout</a>
             </div>
-            <div class="one_btn">
-                <img src="assets/dollar-sign.svg" alt="">
-                <a href="#">Currency</a>
+            <div class="hidden">
+                <p>hidden</p>
             </div>
-            <div class="one_btn">
-                <img src="assets//credit-card.svg" alt="">
-                <a href="#">Payments</a>
-            </div>
-        </div>
-        <div class="container">
-            <a href="logout.php" class="btn" style="text-align:center; width: 90px;">logout</a>
         </div>
 </body>
 
